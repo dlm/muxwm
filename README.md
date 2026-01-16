29ca0c532d51aee6fd8a26b7db44166475b48956
David Millman <dave@cs.unc.edu>
2026-01-16 13:10:25 -0700

    chore: add basic project structure

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # muxwm
-A window-manager workflow layer that provides tmux-like project, task, and pin semantics on top of i3 (and compatible WMs), without replacing the window manager.
+
+A window-manager workflow layer that provides tmux-like project, task, and pin
+semantics on top of i3 (and compatible WMs), without replacing the window
+manager.
---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "A window-manager workflow layer that provides tmux-like semantics";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rustToolchain = pkgs.rust-bin.stable.latest.default;
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "muxwm";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            rustToolchain
+            pkgs.nixfmt-rfc-style
+            pkgs.jujutsu
+          ];
+
+          shellHook = ''
+            echo "Rust development shell ready 🦀"
+          '';
+        };
+      }
+    );
+}
